f35beabb0a8f20b64b1ecef0b2632f6a1ccd1fa6
Weizhe Qu <weizhe.wesley.qu@gmail.com>
2026-07-09 11:32:24 -0700

    title and time

--- src/main.js
+++ src/main.js
@@ -5,6 +5,19 @@ import heroImg from './assets/hero.png'
 import { setupCounter } from './counter.js'
 
 
+function updateClock() {
+    let now = new Date();
+
+    let currTime = now.toLocaleDateString("en-US", { hour: "2-digit", minute: "2-digit", second: "2-digit"});
+    let timeZone = now.getTimezoneOffset();
+
+    document.getElementById("time").innerHTML = `${currTime} ${timeZone}`;
+
+    setTimeout(updateClock, 1000)
+
+}
+
+
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
 document.addEventListener('DOMContentLoaded', () => {
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
@@ -13,23 +26,21 @@ fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
         console.log(data);
         
         document.getElementById("loading").innerHTML = ""
-
-        console.log("waweeee");
         
         
         if (data.media_type == "image") {
-            const image = document.createElement("img")
+            const image = document.createElement("img");
             image.src = data.hdurl;
             image.id = "apod-image";
             image.style.display = "block";
             image.style.margin = "auto"
             image.style.objectFit = "cover";
 
-            document.getElementById("image").appendChild(image)
+            document.getElementById("image").appendChild(image);
         }
 
         else if (data.media_type == "video") {
-            const video = document.createElement("video")
+            const video = document.createElement("video");
             video.src = data.url;
             video.id = "apod-image";
             video.controls = true;
@@ -44,9 +55,13 @@ fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
         } else {
             document.querySelector("#app").innerHTML = "<p>Some weird stuff happened.</p>";
         }
-
+        
+        document.getElementById("title").innerHTML = data.title;
         
     })
 
 
 });
+
+updateClock();
+
