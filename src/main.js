425d431d7103517c3201ec3a435813c60badcf60
Weizhe Qu <weizhe.wesley.qu@gmail.com>
2026-07-20 13:28:19 -0700

    DEF WORKS NOW

--- src/main.js
+++ src/main.js
@@ -120,9 +120,19 @@ fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`) //fetch from url
         formatDate(data.date);
         formatCredits(data.copyright);
 
-        const download = document.getElementById("download")
-        download.href = data.url;
-        download.setAttribute("download", data.url.split("/").pop());
+        fetch(data.url)
+            .then(response => response.blob())
+            .then(blob => {
+                const blobURL = URL.createObjectURL(blob);
+                const download = document.getElementById("download");
+                download.href = blobURL;
+                download.setAttribute("download", data.url.split("/").pop());
+
+                download.addEventListener("click", () => {
+                    URL.revokeObjectURL(blobURL);
+                })
+            })
+
 
         console.log(data.media_type[0].toUpperCase() + data.media_type.slice(1));
 
