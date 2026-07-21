56e9f9f8676e414bba2a562ed563f9c2c8521d4e
Weizhe Qu <weizhe.wesley.qu@gmail.com>
2026-07-21 11:54:57 -0700

    look better

--- src/main.js
+++ src/main.js
@@ -31,17 +31,21 @@ function updateClock() { //function to update the clock every second
 }
 
 function formatCredits(credits) {
-    if (credits.includes(" Text: ")) {
-        let splitCredits = credits.split(" Text: ");
-        let imgCredits = splitCredits[0];
-        let textCredits = splitCredits[1];
-        imgCredits = imgCredits.replace(/\n/g, "");
-        textCredits = textCredits.replace(/\n/g, "");
-        document.getElementById("credits").innerHTML = `${imgCredits}, Text: ${textCredits}`;
-    } else {
-        let formattedCredits = credits.replace(/\n/g, "");
-        document.getElementById("credits").innerHTML = formattedCredits;
+    if (credits) {
+        if (credits.includes(" Text: ")) {
+            let splitCredits = credits.split(" Text: ");
+            let imgCredits = splitCredits[0];
+            let textCredits = splitCredits[1];
+            imgCredits = imgCredits.replace(/\n/g, "");
+            textCredits = textCredits.replace(/\n/g, "");
+            document.getElementById("credits").innerHTML = `${imgCredits}, Text: ${textCredits}`;
+        } else {
+            let formattedCredits = credits.replace(/\n/g, "");
+            document.getElementById("credits").innerHTML = formattedCredits;
 
+        }
+    } else {
+        document.getElementById("credits").innerHTML = "No copyright"
     }
 }
 
